3d054ebff724f95edab0cddb1b2b08255aa0c170
raviraja0969 <raviraja00979@gmail.com>
2023-09-15 19:12:08 +0530

    Added new file

--- mongodb.sh
+++ mongodb.sh
@@ -35,3 +35,23 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB repo into yum.repos.d"
 
+yum install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installation of MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Edited MongoDB conf"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MonogoDB"
+
